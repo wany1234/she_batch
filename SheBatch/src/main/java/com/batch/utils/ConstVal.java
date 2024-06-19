ad543adc17c0e314661aa30a868e828b51efdeaf
wany <bosswany@daum.net>
2024-06-19 17:29:24 +0900

    최신개정법규 OC 생성

--- SheBatch/src/main/java/com/batch/utils/ConstVal.java
+++ SheBatch/src/main/java/com/batch/utils/ConstVal.java
@@ -23,22 +23,15 @@ public class ConstVal {
     public static String ELAW_TYPE = "LAW_TYPE";
 
     /**
-     * 입법예고 - 소관부처 코드
-     * 1492000: 고용노동부
-     * 1721000: 과학기술정보통신부
-     * 1450000: 산업통상자원부
-     * 1661000: 소방청
-     * 1530000: 해양경찰청
-     * 1192000: 해양수산부
-     * 1741000: 행정안전부
-     * 1480000: 환경부
+     * 입법예고 - 소관부처 코드 1492000: 고용노동부 1721000: 과학기술정보통신부 1450000: 산업통상자원부
+     * 1661000: 소방청 1530000: 해양경찰청 1192000: 해양수산부 1741000: 행정안전부 1480000: 환경부
      */
     public static String LAW_MAKING_ORG = "SHE_GOV_TYPE";
 
     /**
      * 법제처 인터페이스 ID
      */
-    public static String LAW_IF_ID = "hyunkchm";
+    public static String LAW_IF_ID = "heesu3923";
 
     /**
      * 알람메일 발송 배치 코드 - 공정안전문서
